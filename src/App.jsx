481b61822e466b52ca4ea9477d39560a77241208
Vitaliy <i@ovitalyo.ru>
2023-12-13 19:08:10 +0300

    one commit

---
+++ src/App.jsx
@@ -0,0 +1,77 @@
+import {useState} from 'react'
+
+import './App.css'
+
+function App() {
+    const [value, setValue] = useState('0'); //expression
+    const [state, setState]= useState(false); // isExtendedMode
+    // if(value.endsWith('+'| '-')){
+    //     return
+    // }
+    // const buttons = [DE,OFF,3,4,5]
+    const myFunc = e => setValue(value + e.target.value) //changeExpression
+    console.log(value)
+    return (
+        <div className='wrapper'>
+            {/*<h1 className='calc'>Калькулятор React</h1>*/}
+            <div className='calc'>
+                <div>
+                    <div className='input'>{value}</div>
+                </div>
+                <div>
+                    {/*добавить симантику*/}
+                    <button onClick={() => setState(!state)}>Func</button>
+                        <input type='button' value="DE" onClick={ ()=>setValue(value.slice(0,-1))}/>
+                        <input type='button' value="OFF" onClick={ ()=>setValue('')}/>
+                        <input type='button' value="/" onClick={myFunc}/>
+                        {state === true && <input type='button' value="Fa" onClick={e => setValue(value + e.target.value)}/>}
+
+                    </div>
+                    <div>
+                        <input type='button' value="7" onClick={myFunc}/>
+                        <input type='button' value="8" onClick={myFunc}/>
+                        <input type='button' value="9" onClick={myFunc}/>
+                        <input type='button' value="*" onClick={myFunc}/>
+                        {state === true && <input type='button' value="Da" onClick={e => setValue(value + e.target.value)}/>}
+                    </div>
+                    <div>
+                        <input type='button' value="4" onClick={e => setValue(value +e.target.value)}/>
+                        <input type='button' value="5" onClick={e => setValue(value +e.target.value)}/>
+                        <input type='button' value="6" onClick={e => setValue(value +e.target.value)}/>
+                        <input type='button' value="+" onClick={e => setValue(value + e.target.value)}/>
+                        {state === true && <input type='button' value="Net" onClick={e => setValue(value + e.target.value)}/>}
+                    </div>
+                    <div>
+                        <input type='button' value="1" onClick={e => setValue(value +e.target.value)}/>
+                        <input type='button' value="2" onClick={e => setValue(value +e.target.value)}/>
+                        <input type='button' value="3" onClick={e => setValue(value +e.target.value)}/>
+                        <input type='button' value="-" onClick={e => setValue(value + e.target.value)}/>
+                        {state === true && <input type='button' value="Get" onClick={e => setValue(value + e.target.value)}/>}
+                    </div>
+                    <div>
+                        <input type='button' value="00" onClick={e => setValue(value +e.target.value)}/>
+                        <input type='button' value="0" onClick={e => setValue(value +e.target.value)}/>
+                        <input type='button' value="." onClick={e => setValue(value + e.target.value)}/>
+                        <input type='button' value="="  onClick={e=> setValue(eval(value))}/>
+                        {/*eval*/}
+                        {state === true && <input type='button' value="Ya" onClick={e => setValue(value + e.target.value)}/>}
+                    </div>
+            </div>
+
+            {/*<br/>*/}
+            {/*<div className="calc">*/}
+            {/*    <button onClick={() => setCount((count) => count + 1)}>*/}
+            {/*        count is {count}*/}
+            {/*    </button>*/}
+            {/*    <p>*/}
+            {/*        Edit <code>src/App.jsx</code> and save to test HMR*/}
+            {/*    </p>*/}
+            {/*</div>*/}
+            {/*<p className="read-the-docs">*/}
+            {/*    Click on the Vite and React logos to learn more*/}
+            {/*</p>*/}
+        </div>
+    )
+}
+
+export default App
